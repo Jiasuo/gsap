0f1590b2775d7b7e1ff7a80bde1467054eec4d5e
jiasuo <jiasuo91@gmail.com>
2022-06-04 12:20:01 +0200

    created project

---
+++ src/GSApp.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {gsap} from "gsap"
+import { onMounted } from "vue";
+
+onMounted(() => {
+  gsap.from(".hi", {
+    y: -300,
+    ease: "bounce",
+    duration: 2
+  })
+})
+</script>
+
+<template>
+<div class="body">
+  <h1 class="hi">Hello world</h1>
+</div>
+</template>
+
+<style>
+.body{
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Poppins", arial, sans-serif;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import GSApp from './GSApp.vue'
+
+createApp(GSApp).mount('#app')
